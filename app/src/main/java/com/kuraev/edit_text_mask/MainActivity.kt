f71f37132c6b6b18e8211f7ae2ddfb1369a5a621
Daniil Kuraev <Daniil.Kuraev@waveaccess.global>
2022-10-11 01:22:32 +0300

    added project

---
+++ app/src/main/java/com/kuraev/edit_text_mask/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.kuraev.edit_text_mask
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
