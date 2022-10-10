f28667ca01c5731d4f6f846e8838679afe42f7af
Daniil Kuraev <Daniil.Kuraev@waveaccess.global>
2022-10-11 01:39:05 +0300

    added library

--- app/src/main/java/com/kuraev/edit_text_mask/example/MainActivity.kt
+++ app/src/main/java/com/kuraev/edit_text_mask/example/MainActivity.kt
@@ -1,7 +1,7 @@
-package com.kuraev.edit_text_mask
+package com.kuraev.edit_text_mask.example
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
